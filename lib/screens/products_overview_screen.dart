db42c4c038de1814b089ff633784646f6e332e44
jonik2909 <boronovjonibek@gmail.com>
2025-06-10 07:52:02 +0900

    feat: publish appBar & BadgeCard Widget

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:car_shop/widgets/badge.dart';
+import 'package:flutter/material.dart';
+
+enum FilterOptions { favorites, all }
+
+class ProductsOverviewScreen extends StatelessWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          "MyShop",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        actions: <Widget>[
+          PopupMenuButton<FilterOptions>(
+            onSelected: (selectedValue) {},
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (ctx) => [
+              PopupMenuItem(
+                child: Text(
+                  "Only favorites",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                value: FilterOptions.favorites,
+              ),
+              PopupMenuItem(
+                child: Text(
+                  "Show all",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                ),
+                value: FilterOptions.all,
+              ),
+            ],
+          ),
+          BadgeCard(value: '5')
+        ],
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
---
+++ lib/widgets/badge.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class BadgeCard extends StatelessWidget {
+  final String value;
+  const BadgeCard({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.add_shopping_cart,
+          ),
+        ),
+        Positioned(
+          right: 3,
+          top: 0,
+          child: Container(
+            padding: EdgeInsets.all(2),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.blue,
+            ),
+            constraints: const BoxConstraints(minWidth: 20, minHeight: 20),
+            child: Center(
+              child: Text(
+                value,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
